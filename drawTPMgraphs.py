c72212cbd5398dc2b6a86d23c124d0225303169c
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2024-06-09 14:44:22 +0100

    Added export to TPM graph

--- drawTPMgraphs.py
+++ drawTPMgraphs.py
@@ -5,13 +5,13 @@ import matplotlib.dates as mdates
 import argparse
 import re
 
-def plot_graph(files):
-    fig, ax = plt.subplots(figsize=(7,4))  # Set the figure size
+def plot_graph(files, output_file=None, export_only=False):
+    fig, ax = plt.subplots(figsize=(7, 4))  # Set the figure size
 
     for file in files:
         data = pd.read_csv(file)
 
-        # Convert the 'Timestamp' column to datetime
+        # Convert the 'time' column to datetime
         data['time'] = pd.to_datetime(data['time'], unit='s')
 
         # Aggregate the data by minute and compute the average TPM
@@ -20,7 +20,7 @@ def plot_graph(files):
 
         vUsers = re.search(r'\d+', file).group()
 
-        ax.plot(data['time'], data['TPM'], label= vUsers + " vusers")
+        ax.plot(data['time'], data['TPM'], label=vUsers + " vusers")
 
     # Set the title and labels
     ax.set_title('TPM during execution')
@@ -29,7 +29,7 @@ def plot_graph(files):
 
     # Set xticks every minute
     ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=1))   # To get a tick every minute
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%M'))     # Tick format
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%M'))       # Tick format
 
     # Add a legend and place it below the plot with horizontal layout
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=len(files), fancybox=True, shadow=True)
@@ -37,15 +37,22 @@ def plot_graph(files):
     # Adjust the plot to make room for the legend
     plt.tight_layout()
 
-    # Show the plot
-    plt.show()
+    # Save the plot to a PDF file if output_file is specified
+    if output_file:
+        plt.savefig(output_file, format='pdf')
+
+    # Show the plot if export_only is False
+    if not export_only:
+        plt.show()
 
 def main():
     parser = argparse.ArgumentParser(description='Plot graph from CSV files.')
     parser.add_argument('files', nargs='+', help='The CSV files to plot')
+    parser.add_argument('-output', help='The output PDF file')
+    parser.add_argument('-export', action='store_true', help='Only export the graph to a PDF, do not display it')
     args = parser.parse_args()
 
-    plot_graph(args.files)
+    plot_graph(args.files, args.output, args.export)
 
 if __name__ == "__main__":
     main()
